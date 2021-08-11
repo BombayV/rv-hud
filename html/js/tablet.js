977d9f11ebfff7075ecc6190863d89db7c9f5980
XiosBombay <OMGmsebastian@gmail.com>
2021-08-10 22:39:39 -0500

    feat(ui/header): added time

--- html/js/tablet.js
+++ html/js/tablet.js
@@ -12,10 +12,13 @@ doc.querySelector('.tablet-night-btn').addEventListener('click', () => {
 });
 
 const restoreTablet = () => {
+    // Darkmode Saving
     if (null != getBool('currentMode')) {currentMode = getBool('currentMode')}
     if (getBool('currentMode')) {
         doc.body.classList.add('dark-mode');
     } else {
         doc.body.classList.remove('dark-mode');
     }
+
+    //
 }
