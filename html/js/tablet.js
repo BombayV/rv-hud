17b84db92ec1feef4d54b09c420bff2da73f1877
XiosBombay <OMGmsebastian@gmail.com>
2021-08-11 23:06:33 -0500

    feat(js/sidebar): added saving of sidebar

--- html/js/tablet.js
+++ html/js/tablet.js
@@ -11,12 +11,14 @@ doc.querySelector('.tablet-night-btn').addEventListener('click', () => {
 sidebar.addEventListener('click', () => {
     const par = sidebar.parentNode;
     barStatus ? (barStatus = false, par.style.left = "-0.5%", par.style.background = "var(--opaque-dark)", par.style.borderRight = "0.4vh solid var(--border-dark)", par.style.transition = "left 0s") : (barStatus = true, par.style.left = "-20.5%", par.style.background = "transparent", par.style.borderRight = "none", par.style.transition = "left 0.1s")
-    
+    storeId('barStatus', barStatus)
 })
 
 const restoreTablet = () => {
-    // Darkmode Saving
+    // Darkmode
     getBool('currentMode') != null ? (currentMode = getBool('currentMode'), getBool('currentMode') ? doc.body.classList.add('dark-mode') : doc.body.classList.remove('dark-mode')) : currentMode;
 
-    //
+    // Sidebar
+    const par = sidebar.parentNode;
+    getBool('barStatus') != null ? (barStatus = getBool('barStatus'), getBool('barStatus') ? (par.style.left = "-20.5%", par.style.background = "transparent", par.style.borderRight = "none", par.style.transition = "left 0.1s") : (par.style.left = "-0.5%", par.style.background = "var(--opaque-dark)", par.style.borderRight = "0.4vh solid var(--border-dark)", par.style.transition = "left 0s")) : barStatus;
 }
