d9dd3c5c8b9034591ef4bb4a57869b2522fdacaf
XiosBombay <OMGmsebastian@gmail.com>
2021-08-11 21:10:31 -0500

    feat(ui/sidebar): created functional sidebar

--- html/js/tablet.js
+++ html/js/tablet.js
@@ -1,9 +1,18 @@
-let currentMode = false
+const sidebar = doc.getElementById('sidebar');
+
+let currentMode = false;
+let barStatus = false;
 
 doc.querySelector('.tablet-night-btn').addEventListener('click', () => {
     currentMode ? (currentMode = false, doc.body.classList.remove('dark-mode')) : (currentMode = true, doc.body.classList.add('dark-mode'));
     storeId('currentMode', currentMode);
 });
+
+sidebar.addEventListener('click', () => {
+    const par = sidebar.parentNode;
+    barStatus ? (barStatus = false, par.style.left = "-0.5%", par.style.background = "var(--opaque-dark)", par.style.borderRight = "0.4vh solid var(--border-dark)") : (barStatus = true, par.style.left = "-20.5%", par.style.background = "transparent", par.style.borderRight = "none")
+    
+})
 
 const restoreTablet = () => {
     // Darkmode Saving
