5aee5415acfb7c1b175087c24fa72279d8b68218
XiosBombay <OMGmsebastian@gmail.com>
2021-08-11 22:47:27 -0500

    chore(ui/sidebar): set hidden to default

--- html/js/tablet.js
+++ html/js/tablet.js
@@ -1,7 +1,7 @@
 const sidebar = doc.getElementById('sidebar');
 
 let currentMode = false;
-let barStatus = false;
+let barStatus = true;
 
 doc.querySelector('.tablet-night-btn').addEventListener('click', () => {
     currentMode ? (currentMode = false, doc.body.classList.remove('dark-mode')) : (currentMode = true, doc.body.classList.add('dark-mode'));
@@ -10,7 +10,7 @@ doc.querySelector('.tablet-night-btn').addEventListener('click', () => {
 
 sidebar.addEventListener('click', () => {
     const par = sidebar.parentNode;
-    barStatus ? (barStatus = false, par.style.left = "-0.5%", par.style.background = "var(--opaque-dark)", par.style.borderRight = "0.4vh solid var(--border-dark)", par.style.transition = "left 0s") : (barStatus = true, par.style.left = "-20.5%", par.style.background = "transparent", par.style.borderRight = "none", par.style.transition = "left 0.2s")
+    barStatus ? (barStatus = false, par.style.left = "-0.5%", par.style.background = "var(--opaque-dark)", par.style.borderRight = "0.4vh solid var(--border-dark)", par.style.transition = "left 0s") : (barStatus = true, par.style.left = "-20.5%", par.style.background = "transparent", par.style.borderRight = "none", par.style.transition = "left 0.1s")
     
 })
 
