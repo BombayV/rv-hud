cade9b52f7a21a004b8378408bb20148db218acb
XiosBombay <OMGmsebastian@gmail.com>
2021-08-17 23:52:54 -0500

    feat(client/voice): added pma export

--- modules/client/hud.lua
+++ modules/client/hud.lua
@@ -1,6 +1,18 @@
 function currentVoiceMode(value)
-    SendNUIMessage({
-        action = 'voiceMode',
-        mode = value
-    })
+    if (value == 1) then
+        SendNUIMessage({
+            action = 'voiceMode',
+            microphone = 33
+        })
+    elseif (value == 2) then
+        SendNUIMessage({
+            action = 'voiceMode',
+            microphone = 66
+        })
+    elseif (value == 3) then
+        SendNUIMessage({
+            action = 'voiceMode',
+            microphone = 100
+        })
+    end
 end
